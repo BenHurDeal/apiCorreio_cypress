e43d0614be7c8d24cc08c50786d35701f90604df
BenHurJF <beiujeffer@hotmail.com>
2023-05-03 02:56:34 -0300

    Testes implementados com sucesso :), Rápido e eficaz.

--- cypress/integration/apiCorreio.spec.js
+++ cypress/integration/apiCorreio.spec.js
@@ -1,7 +1,65 @@
 /// <reference types="cypress" />
 
-describe('Cenário de api', () => {
-    it('', () => {
-        
+import objetoCapitaisCep from '../fixtures/capitaisCep.json';
+import arrayResponseCapitais from '../fixtures/responsesCapitais.json';
+
+describe('Cenário de testes da api cep-correios', () => {
+
+    Object.keys(objetoCapitaisCep).forEach((capitalAtual) => {
+        it(`Valida dados do CEP da capital: ${capitalAtual}`, { env: { hideCredentials: true } }, () => {
+            cy.request({
+                method: 'GET',
+                url: Cypress.env('baseUrl') + `${objetoCapitaisCep[capitalAtual]}`,
+                headers: { Cookie: Cypress.env('Cookie') }
+            }).then(({ status, body }) => {
+                const {
+                    address, restrictions, number, city, longitude, state,
+                    id, complement, latitude, neighborhood, blockDelivery } = body;
+
+                const filtrarCapitalAtual = arrayResponseCapitais.filter((responseCapitalFixture) => responseCapitalFixture.city === capitalAtual)
+                filtrarCapitalAtual.forEach((matchCapital) => {
+
+                    expect(status).to.be.eq(200)
+                    expect(address).to.be.eq(matchCapital.address)
+                    expect(restrictions).to.be.deep.eq(matchCapital.restrictions)
+                    expect(number).to.be.eq(matchCapital.number)
+                    expect(city).to.be.eq(matchCapital.city)
+                    expect(longitude).to.be.eq(matchCapital.longitude)
+                    expect(state).to.be.eq(matchCapital.state)
+                    expect(id).to.be.eq(matchCapital.id)
+                    expect(complement).to.be.eq(matchCapital.complement)
+                    expect(latitude).to.be.eq(matchCapital.latitude)
+                    expect(neighborhood).to.be.eq(matchCapital.neighborhood)
+                    expect(blockDelivery).to.be.eq(matchCapital.blockDelivery)
+                })
+            })
+        })
     });
+
+    context('Fluxos inválidos', () => {
+
+        it('CEP com traço', () => {
+            Cypress.Commands.add('getCep', (cep, msg, sttsCode) => {
+                cy.request({
+                    method: 'GET',
+                    url: Cypress.env('baseUrl') + cep,
+                    failOnStatusCode: false
+                }).then(({ status, body }) => {
+                    expect(status).to.equal(sttsCode)
+                    expect(body.message).to.equal(msg)
+                    expect(body.errorCode).to.equal('400')
+                })
+            })
+            cy.getCep('66917-190', 'Requisição mal formatada', 400)
+        });
+
+        it('CEP com letras', () => {
+            cy.getCep('6D6TR43W', 'Requisição mal formatada', 400)
+        });
+
+        it('CEP vazio', () => {
+            cy.getCep('', 'Parâmetro CEP obrigatório', 400)
+        });
+    })
+
 });
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,6 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import 'cypress-plugin-api';
+
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
